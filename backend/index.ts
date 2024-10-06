d628db88c6bd45ea349150704c2de92ed4d37d87
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-10-07 01:16:04 +0200

    Handle cors a bit better

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,39 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "./router";
 
+const addCORSHeaders = (res: Response) => {
+  const response = new Response(res.body, res);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Headers", "*");
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  return response;
+};
+
+const handleCORSPreflight = () => {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    },
+  });
+};
+
 export default {
   async fetch(request: Request): Promise<Response> {
-    return fetchRequestHandler({
-      endpoint: "/trpc",
+    if (request.method === "OPTIONS") {
+      return handleCORSPreflight();
+    }
+
+    const response = await fetchRequestHandler({
+      endpoint: "/",
       req: request,
       router: appRouter,
-      createContext: () => ({}),
-      responseMeta: () => {
-        return {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-          },
-        };
-      },
     });
+
+    return addCORSHeaders(response);
   },
 };
